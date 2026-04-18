e6926bb573cd67ac6bac4dea410a8f8337bd7d69
Prince Agrawal <princeagrawal024@gmail.com>
2026-04-18 21:29:30 +0530

    Initial commit - Review Genius Agent setup

---
+++ src/main/java/com/reviewgenius/agent/ReviewGeniusAgentApplication.java
@@ -0,0 +1,13 @@
+package com.reviewgenius.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewGeniusAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewGeniusAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviewgenius/agent/ReviewGeniusAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewgenius.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewGeniusAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
